4575ad4acca498e125a8045cf46af9f50e39afec
Ville Aaltonen <ville.v.aaltonen@gmail.com>
2016-09-10 16:38:11 +0300

    Add cors

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
+var cors = require('cors');
+
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', routes);
 
 app.use(function(req, res, next) {
@@ -41,5 +43,5 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + port);
 });
--- src/actions/Movies.js
+++ src/actions/Movies.js
@@ -19,9 +19,7 @@ export function getAllMovies(dispatch) {
     return function(dispatch, getState) {
         let state = getState()
 
-        return fetch(host, {
-            mode: 'no-cors'
-        })
+        return fetch(host)
             .then((response) => {
                 if(response.ok) {
                     return response.json()
